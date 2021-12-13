bb170a16a9ce40defdf1ff7c36c6f17908112977
Abhin <shahabhin27@gmail.com>
2021-12-13 10:42:55 +0530

    - rebase with master

--- app/src/main/java/com/loyaltyglobal/ui/main/view/activity/MainActivity.kt
+++ app/src/main/java/com/loyaltyglobal/ui/main/view/activity/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        homeViewModel.getSubBrands()
-        homeViewModel.getUserPassFromAgency()
+//        homeViewModel.getSubBrands()
+//        homeViewModel.getUserPassFromAgency()
     }
 }
